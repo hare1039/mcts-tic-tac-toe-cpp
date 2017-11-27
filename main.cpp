636b68ebf7789f2125a706f5cee21205caa74ccc
hare1039 <hare39393@gmail.com>
2017-11-28 00:29:34 +0800

    Make simulation longer

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     namespace mcts = monte_carlo_tree_search;
     mcts::Node::peice p = mcts::Node::O;
     mcts::Tree tree(p);
-    for(int i(0); i<1000000; i++)
+    for(int i(0); i<1000000000; i++)
     {
         mcts::Node * node = tree.root.best_node();
         node->random_play();
